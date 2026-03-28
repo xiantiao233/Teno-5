672bde38e1e2f9200faf24e27b58e2f4caf85e28
xiantiao <my@xiantiao.fun>
2026-03-28 09:54:55 +0800

    1

---
+++ Debug/sources.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+ELF_SRCS := 
+OBJ_SRCS := 
+S_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+CYCLO_FILES := 
+OBJCOPY_HEX := 
+SIZE_OUTPUT := 
+OBJDUMP_LIST := 
+SU_FILES := 
+EXECUTABLES := 
+OBJS := 
+MAP_FILES := 
+S_DEPS := 
+S_UPPER_DEPS := 
+C_DEPS := 
+OBJCOPY_BIN := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Core/Src \
+Core/Startup \
+Drivers/STM32G4xx_HAL_Driver/Src \
+Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/App \
+Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Class/CDC_ACM/Src \
+Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Class/COMPOSITE/Src \
+Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Class/HID_KEYBOARD/Src \
+Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Core/Src \
+Middlewares/Third_Party/AL94_USB_Composite/COMPOSITE/Target \
+
